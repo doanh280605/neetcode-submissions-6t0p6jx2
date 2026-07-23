e9848c2b6dfe029c141bb3aa1161692f002b10a3
Doanh Phung <87482217+doanh280605@users.noreply.github.com>
2026-07-23 00:20:03 -0700

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * (n + 1)
+
+        if n == 0: 
+            return 0
+
+        dp[0] = nums[0]
+        dp[1] = nums[1]
+
+        for i in range(n): 
+            skip = dp[i - 1] # if we choose to skip the current house
+            # what is the best value up to house i - 1? 
+
+            # but if we choose to rob the current house
+            # what is the best value up to house i - 2 (not adjacent house) + this cur house?
+            rob = dp[i - 2] + nums[i]
+
+            dp[i] = max(skip, rob) # then pick between them which path is better to go, skip or rob
+        
+        return dp[n - 1]
